2b530746729f4aa63059d12747d94bf8b9f7ddf2
himanshiLt <89007259+himanshiLt@users.noreply.github.com>
2021-12-23 13:06:43 +0530

    Create new-test.js

---
+++ test/algorithms/new-test.js
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import casual from 'casual';
+import geometric from '../../src/algorithms/geometric';
+
+describe('New algorithm', () => {
+  it('should have inverse functions for getPosition and getValue', () => {
+    const min = casual.integer(0, 100);
+    const max = casual.integer(900, 1000);
+    const originalPosition = casual.integer(100, 900);
+    const value = geometric.getValue(originalPosition, min, max);
+    const positionFromValue = geometric.getPosition(value, min, max);
+    assert.equal(Math.round(positionFromValue), originalPosition);
+  });
+});
