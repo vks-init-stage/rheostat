c5ea885c6957d4b59cc7e597cccebb9742de6ddb
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-01-05 11:56:24 +0530

    Remove test suite

--- test/algorithms/geometric-test.js
+++ test/algorithms/geometric-test.js
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import casual from 'casual';
 import geometric from '../../src/algorithms/geometric';
 
-describe('geometric algorithm', () => {
+//describe('geometric algorithm', () => {
   it('should have inverse functions for getPosition and getValue', () => {
     const min = casual.integer(0, 100);
     const max = casual.integer(900, 1000);
@@ -58,7 +58,7 @@ describe('geometric algorithm', () => {
     assert.equal(positionFromValue, x);
   });
  
-});
+//});
 
 describe('new test suite geometric algorithm', () => {
   it('should have inverse functions for getPosition and getValue', () => {
