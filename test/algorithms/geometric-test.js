a411f93855a64e06d8fd83ea981213b764ae8e77
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-01-05 11:45:53 +0530

    Add new test suite

--- test/algorithms/geometric-test.js
+++ test/algorithms/geometric-test.js
@@ -59,3 +59,15 @@ describe('geometric algorithm', () => {
   });
  
 });
+
+describe('new test suite geometric algorithm', () => {
+  it('should have inverse functions for getPosition and getValue', () => {
+    const min = casual.integer(0, 100);
+    const max = casual.integer(900, 1000);
+    const originalPosition = casual.integer(100, 900);
+    const value = geometric.getValue(originalPosition, min, max);
+    const positionFromValue = geometric.getPosition(value, min, max);
+    assert.equal(Math.round(positionFromValue), originalPosition);
+  });
+});
+
