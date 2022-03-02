8eace1f01862a5ccd7764626676627c41b1091a5
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-03-02 15:41:46 +0530

    Update geometric.js

--- src/algorithms/geometric.js
+++ src/algorithms/geometric.js
@@ -3,6 +3,7 @@ export default {
     return ((m / (m - mi)) ** 0.5) * (((xy - mi) / m) ** 0.5) * 100;
   },
 
+  
   getValue(x, min, max) {
     return (Math.round(((x / 100) ** 2) * (max - min)) + min);
   },
